16af4af9bb219c168a1d7dcc1b1e1a5ca233b421
Collins Kibet <kollcibe05@gmail.com>
2025-11-21 22:16:21 +0300

    many-to-many relationship

--- OOP.py
+++ OOP.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # class Shopper:
 #     def __init__(self, name):
 #         self.name = name
@@ -147,3 +149,35 @@ class Child:
 
 # [print(c.name) for c in parent1.children()]
 
+class Student:
+    all = []
+    def __init__(self, name):
+        self.name = name
+        Student.all.append(self)
+    def enroll_in_course(self, course):
+        Enrollment(self, course)  
+    def enrollments(self):
+        return [enrollment for enrollment in Enrollment.all if enrollment.student == self]      
+    def courses(self):
+        return [enrollment.course for enrollment in self.enrollments()]  
+
+class Course:
+    all = []
+    def __init__(self, title):
+        self.title = title
+        Course.all.append(self)  
+    def enrollments(self):
+        return [enrollment for enrollment in self.enrollments()]             
+    def students(self):
+        return [enrollment.student for enrollment in self.enrollments()]
+    def enroll_student(self, student):
+        Enrollment(student, self)  
+
+class Enrollment:
+    all = []
+
+    def __init__(self, student, course):
+        self.student = student
+        self.course = course
+        self.enrollment_date = datetime.now()
+        Enrollment.all.append(self)              
