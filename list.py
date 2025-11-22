5b1b9e5d39990a685a6d8f4e44386b7a33b564c8
Collins Kibet <kollcibe05@gmail.com>
2025-11-22 12:05:41 +0300

    Yooo! _ in python is legittgit add .

--- list.py
+++ list.py
@@ -4,3 +4,18 @@ last_element = numbers[-1]
 
 squared_numbers = [num**2 for num in numbers]
   
+
+######Yo!!! _ in Python is crazy####
+print(1_000_000) 
+
+for _ in range(5):
+    print("Hello!!!!")
+
+##in REPL ##
+# 15 + 15 <enter>
+# _ + 15 #=> 45!!!!
+
+
+# Also used in match__case to refer to any other case
+
+# Also to use a reserved word to avoid conflicts eg. class_
