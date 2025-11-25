fd65c0ce210b4f8982dc1c1e2f7247774c516b78
Collins Kibet <kollcibe05@gmail.com>
2025-11-25 12:56:12 +0300

    outliner

--- testing_stuff.py
+++ testing_stuff.py
@@ -8,4 +8,15 @@ for name in list_example:
         my_dict[name] += 1
     else:
         my_dict[name] = 1    
-print(my_dict)    
+print(my_dict)  
+
+even_remove_odd = [2, 4, 0, 100, 4, 11, 2602, 36] #-->  11 (the only odd number)
+
+odd_remove_even = [160, 3, 1719, 19, 11, 13, -21] #--> 160 (the only even number)
+
+
+
+def find_outlier(integers):
+    pass
+
+# print(find_outlier(even_remove_odd))    
